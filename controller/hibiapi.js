4f9c7f0aeb49a0a7f3b4a704921d28c8486b0f8c
Unknown <ankitsocial16@gmail.com>
2017-09-13 12:55:36 +0530

    adjusted links

--- controller/hibiapi.js
+++ controller/hibiapi.js
@@ -25,99 +25,99 @@ app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html');
 });
 //login test
-app.post('/api/hibi/login_test', function(req,res){
+app.post('//login_test', function(req,res){
   login_test(req.body,function(data){
   res.json(data);
   res.end()
  });
 });
 //NoticeBoard
-app.post('/api/hibi/notice', function(req,res){
+app.post('/notice', function(req,res){
   notice(req.body,function(data){
   res.json(data);
  });
 });
 //notice_data
-app.post('/api/hibi/notice_data', function(req,res){
+app.post('/notice_data', function(req,res){
   
     notice_data(req.body,function(data){
     res.json(data);
   });
 });
 //mycourse
-app.post('/api/hibi/mycourse', function(req,res){
+app.post('/mycourse', function(req,res){
     mycourse(req.body,function(data){
     res.json(data);
   });
 });
 //course_details
-app.post('/api/hibi/course_details', function(req,res){
+app.post('/course_details', function(req,res){
     course_details(req.body,function(data){
     res.json(data);
   });
 });
 //attendence
-app.post('/api/hibi/attendence', function(req,res){
+app.post('/attendence', function(req,res){
     attendence(req.body,function(data){
     res.json(data);
   });
 });
 //course_noticeboard
-app.post('/api/hibi/course_notice', function(req,res){
+app.post('/course_notice', function(req,res){
     course_notice(req.body,function(data){
     res.json(data);
   });
 });
 //course_noticeboard_data
-app.post('/api/hibi/course_notice_data', function(req,res){
+app.post('/course_notice_data', function(req,res){
     course_notice_data(req.body,function(data){
     res.json(data);
   });
 });
 //view_grades
-app.post('/api/hibi/view_grades', function(req,res){
+app.post('/view_grades', function(req,res){
     view_grades(req.body,function(data){
     res.json(data);
   });
 });
 // sub_grades
-app.post('/api/hibi/sub_grades', function(req,res){
+app.post('/sub_grades', function(req,res){
     sub_grades(req.body,function(data){
     res.json(data);
   });
 });
 // fee
-app.post('/api/hibi/fee', function(req,res){
+app.post('/fee', function(req,res){
     fee(req.body,function(data){
     res.json(data);
   });
 });
 // library books
-app.post('/api/hibi/library_books', function(req,res){
+app.post('/library_books', function(req,res){
     library_books(req.body,function(data){
     res.json(data);
   });
 });
 // ebooks
-app.post('/api/hibi/ebooks', function(req,res){
+app.post('/ebooks', function(req,res){
     ebooks(req.body,function(data){
     res.json(data);
   });
 });
 // ebook data
-app.post('/api/hibi/ebook_data', function(req,res){
+app.post('/ebook_data', function(req,res){
     ebook_data(req.body,function(data){
     res.json(data);
   });
 });
 // student
-app.post('/api/hibi/student', function(req,res){
+app.post('/student', function(req,res){
     student(req.body,function(data){
     res.json(data);
   });
 });
 // complaint
-app.post('/api/hibi/complaint', function(req,res){
+app.post('/complaint', function(req,res){
     complaint(req.body,function(data){
     res.json(data);
   });
